acf0175a4d2a3eae1c64587394bbe77e9142e031
jmarquesb <jmarquesb@gmail.com>
2018-05-03 14:19:34 +0200

    Next commit.

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,80 @@
+pragma solidity ^0.4.17;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+    
+    function createCampaign(uint minimum) public{
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns(address[]){
+        return deployedCampaigns; 
+    }
+}
+
+contract Campaign {
+    struct Request{
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;    
+    }
+    
+    Request[] public requests;
+    address public manager;
+    uint public minimunContribution;
+    uint public approversCount;
+    mapping(address => bool) public approvers;
+    
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    constructor(uint minimun, address creator) public{
+        manager = creator;
+        minimunContribution = minimun;
+    }
+    
+    function contribute() public payable{
+        require(msg.value > minimunContribution);
+        
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+    
+    function createRequest(string description, uint value, address recipient) public restricted{
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+
+        requests.push(newRequest);
+    }
+    
+    function approveRequest(uint index) public{
+        Request storage request = requests[index];
+        
+        require(approvers[msg.sender]);
+        require(!request.complete);
+        require(!request.approvals[msg.sender]);
+        
+        request.approvalCount++;
+        request.approvals[msg.sender] = true;
+    }
+    
+    function finalizeRequest(uint index) public restricted{
+        Request storage request = requests[index];
+        require(!request.complete);
+        require(request.approvalCount >(approversCount / 2));
+       
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
---
+++ ethereum/deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+    'dizzy adapt jealous ten family flash glance drastic gossip swing error best',
+    'https://rinkeby.infura.io/44uCGTSwP0ja1FzRvCeR');
+
+const web3 = new Web3(provider);
+
+//Necesario para poder usar el async + await!!!!
+const deploy = async () => {
+    const accounts =  await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account: ', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: '0x' + compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contact deployed to: ' , result.options.address);
+};
+deploy();
